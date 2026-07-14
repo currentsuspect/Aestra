7701ac8ce3163ae4cf7ad937b76cf31742927d43
currentsuspect <dylanmakori@gmail.com>
2026-07-15 02:43:03 +0300

    feat(browser): unify adaptive content views

--- Source/Core/AestraContent.h
+++ Source/Core/AestraContent.h
@@ -333,6 +333,8 @@ private:
     std::shared_ptr<Aestra::Audio::PatternBrowserPanel> m_patternBrowser;
     bool m_pluginScanWasRunning{false};
     float m_fileBrowserWidthPref{-1.0f};
+    float m_pluginBrowserWidthPref{-1.0f};
+    float m_patternNavBrowserWidthPref{-1.0f};
     float m_patternBrowserWidthPref{-1.0f};
     bool m_browserResizing{false};
     BrowserResizeTarget m_browserResizeTarget{BrowserResizeTarget::None};
